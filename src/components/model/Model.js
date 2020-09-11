88ee6bc865a08b195c415ac9ddee6f264d334eb6
aartvanempel <aartvanempel@Aarts-Macbook.local>
2020-09-11 10:04:37 +0200

    added unwanted chars

--- src/components/model/Model.js
+++ src/components/model/Model.js
@@ -23,7 +23,7 @@ class Model {
                 .replace("Liveblog", "")
                 .replace("Transferupdates", "")
                 .replace("NUcheckt", "")
-                .replace(/[’,()"':|]/g, '') // remove unwanted chars
+                .replace(/[’,()"':|?!]/g, '') // remove unwanted chars
                 .replace(/\s+/g, ' ') // remove double spaces
         )
     }
